a82d9603c87a4354b0607d6d0125a69b9bddcdf3
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-04-10 21:06:10 +0530

    Demonstrate creating Regions and Modules.

---
+++ ModuleA/ModuleAModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        private IUnityContainer _container;
+        private IRegionManager _regionManager;
+
+        public ModuleAModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            _container = container;
+        }
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("ToolbarRegion", typeof(ToolbarView));
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ContentView));
+        }
+    }
+}
---
+++ RegionDemo/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+using ModuleA;
+
+namespace RegionDemo
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var typeOfModule = typeof(ModuleAModule);
+            ModuleCatalog.AddModule(new ModuleInfo
+            {
+                InitializationMode = InitializationMode.WhenAvailable,
+                ModuleType = typeOfModule.AssemblyQualifiedName,
+                ModuleName = typeOfModule.Namespace
+            });
+        }
+    }
+}
